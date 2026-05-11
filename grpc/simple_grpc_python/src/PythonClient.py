d5d257c4d2beee8822b44bebec097c9008e324ec
f-giacobbe <100165496+f-giacobbe@users.noreply.github.com>
2026-05-11 11:30:32 +0200

    gRPC Python exercises

--- grpc/simple_grpc_python/src/PythonClient.py
+++ grpc/simple_grpc_python/src/PythonClient.py
@@ -1,17 +1,47 @@
 import grpc
 import Greeter_pb2 as Action
 import Greeter_pb2_grpc as Service
+import calculator_pb2_grpc as CalculatorService
+import calculator_pb2 as CalculatorAction
+import rubrica_pb2 as RubricaAction
+import rubrica_pb2_grpc as RubricaService
+
+
+def greeter(channel):
+    name = input("Inserisci il tuo nome: ")
+    print("Invio il tuo nome al server gRPC\n")
+
+    stub = Service.GreeterStub(channel)
+    response = stub.SayHello(Action.HelloRequest(name=name))
+
+    print("Il server gRPC dice: " + response.message)
+
+def calculator(channel):
+    opA = 5
+    opB = 6
+
+    stub = CalculatorService.CalculatorStub(channel)
+    response = stub.Add(CalculatorAction.OperationRequest(opA=opA, opB=opB))
+
+    print("Il server gRPC dice: " + str(response.res))
+
+def rubrica(channel):
+    stub = RubricaService.RubricaStub(channel)
+
+    c1 = RubricaAction.Person(nome="Francesco", cognome="Giacobbe", telefono=123)
+    c2 = RubricaAction.Person(nome="Topolino", cognome="Pippo", telefono=456)
+
+    stub.AddContact(c1)
+    print( stub.GetAll( RubricaAction.ListRequest() ).contatti)
 
 
 def run():
     with grpc.insecure_channel('localhost:50051') as channel:
-        name = input("Inserisci il tuo nome: ")
-        print("Invio il tuo nome al server gRPC\n")
+        # greeter(channel)
 
-        stub = Service.GreeterStub(channel)
-        response = stub.SayHello(Action.HelloRequest(name=name))
+        # calculator(channel)
 
-        print("Il server gRPC dice: " + response.message)
+        rubrica(channel)
 
 
 if __name__ == '__main__':
--- grpc/simple_grpc_python/src/PythonServer.py
+++ grpc/simple_grpc_python/src/PythonServer.py
@@ -2,6 +2,13 @@ from concurrent import futures
 import grpc
 import Greeter_pb2 as Action
 import Greeter_pb2_grpc as Service
+import calculator_pb2_grpc as CalculatorService
+import calculator_pb2 as CalculatorAction
+import rubrica_pb2 as RubricaAction
+import rubrica_pb2_grpc as RubricaService
+
+
+rubrica = {}
 
 
 class Greeter(Service.GreeterServicer):
@@ -9,9 +16,38 @@ class Greeter(Service.GreeterServicer):
         return Action.HelloReply(message="Sono gRPC su python, Ciao " + request.name)
 
 
+class Calculator(CalculatorService.CalculatorServicer):
+    def Add(self, request, context):
+        return CalculatorAction.OperationResponse(res=request.opA+request.opB)
+
+    def Subtract(self, request, context):
+        return CalculatorAction.OperationResponse(res=request.opA-request.opB)
+
+    def Multiply(self, request, context):
+        return CalculatorAction.OperationResponse(res=request.opA*request.opB)
+
+    def Divide(self, request, context):
+        return CalculatorAction.OperationResponse(res=request.opA/request.opB)
+
+
+class Rubrica(RubricaService.RubricaServicer):
+    def AddContact(self, request, context):
+        rubrica[request.nome + request.cognome] = RubricaAction.Person(nome=request.nome, cognome=request.cognome, telefono=request.telefono)
+        return RubricaAction.Result(res=True)
+
+    def GetContact(self, request, context):
+        return RubricaAction.Person(nome=request.nome, cognome=request.cognome, telefono=rubrica[request.nome + request.cognome])
+
+    def GetAll(self, request, context):
+        return RubricaAction.ContactList(contatti=rubrica.values())
+
+
+
 port = "50051"
 server = grpc.server(futures.ThreadPoolExecutor(max_workers=10))
 Service.add_GreeterServicer_to_server(Greeter(), server)
+CalculatorService.add_CalculatorServicer_to_server(Calculator(), server)
+RubricaService.add_RubricaServicer_to_server(Rubrica(), server)
 server.add_insecure_port("localhost:"+port)
 server.start()
 print(f"Listening on port {port}")
